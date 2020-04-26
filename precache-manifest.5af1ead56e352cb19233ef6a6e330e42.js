3e62119e971133bf989e3ed0da159e766c8b71e7
Joe Overton <joeoverton2697@gmail.com>
2020-04-26 11:24:44 -0400

    Updates

---
+++ precache-manifest.5af1ead56e352cb19233ef6a6e330e42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc269b3c907975f0467bf97cecd06bc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12836804ca5534f25478",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4645ec387bfe9740361f",
+    "url": "/static/js/2.d0cf36f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d0cf36f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12836804ca5534f25478",
+    "url": "/static/js/main.9c2cafb5.chunk.js"
+  },
+  {
+    "revision": "b64034b3fbd239c3de40",
+    "url": "/static/js/runtime-main.f9b3addf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/joverton2697.github.io/precache-manifest.40b9ae0822b72dcc22141e613823e612.js"
+  "/precache-manifest.5af1ead56e352cb19233ef6a6e330e42.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/joverton2697.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
